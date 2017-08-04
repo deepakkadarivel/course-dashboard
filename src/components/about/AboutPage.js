93234659e036dc72673883ee20678e4b3d0dbea7
Deepakkv <deepakkv@in-deepakkv.local>
2017-08-05 01:43:40 +0530

    [Deepak] React components created for About, Home & Courses page with Routing.

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>Abput Page</h1>
+      <p>This is the about page with generic details.</p>
+    </div>
+  );
+};
+
+export default AboutPage;
+
---
+++ src/components/course/CoursesPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CoursesPage = () => {
+  return (
+    <div>
+      <h1>Course Page</h1>
+    </div>
+  );
+};
+
+export default CoursesPage;
